1ebd371aa7930482a68e1b0ce3131f2c5a133d6b
aihoon <hoon.paek@mindslab.ai>
2017-03-21 15:55:30 -0700

    Commit

--- cloopin/urls.py
+++ cloopin/urls.py
@@ -18,7 +18,6 @@ from django.contrib import admin
 from django.views.generic import RedirectView
 
 urlpatterns = [
-    url(r'^$', RedirectView.as_view(url='/ivsearch/')),
     url(r'^admin/', admin.site.urls),
     url(r'^$', RedirectView.as_view(url='/mindslab/')),
 
