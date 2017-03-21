527f451339c43e7b006111cbdbc6ddd2fcd0df0a
aihoon <hoon.paek@mindslab.ai>
2017-03-21 15:45:42 -0700

    Commit

--- mindslab/views.py
+++ mindslab/views.py
@@ -1,18 +1,8 @@
 from django.shortcuts import render
 # from django.shortcuts import HttpResponse
-from django.views.generic import TemplateView
-from django.http import HttpResponse
-
-import json
 
 
 def index(request):
     return render(request, 'mindslab/index.html')
 
 
-class PostExample(TemplateView):
-    template_name = 'mindslab/start.html'
-
-    @staticmethod
-    def post(request):
-        return HttpResponse(json.dumps({'key': 'value'}), mimetype="application/json")
