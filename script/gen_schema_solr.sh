383bd3b3e23bc1decf8aa1a00766f33c145a71d7
aihoon <hoon.paek@mindslab.ai>
2017-03-23 11:57:48 -0700

    remove script directory

--- script/gen_schema_solr.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd ..
-python manage.py build_solr_schema > schema.xml
-mv schema.xml solr/solr-4.10.2/example/solr/collection1/conf
-
--- script/run_solr.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd ..
-cd solr/solr-4.10.2/example/;
-java -jar start.jar 
-
